44918773ca9f8b1211c012dc8c8a9bf1ed70ac28
Thales Henrique <thalesh1997@gmail.com>
2023-01-05 11:19:17 -0300

    adicionado secao de respostas

--- notebooks/app.py
+++ notebooks/app.py
@@ -40,8 +40,8 @@ def create_dataframe_section(df):
 
 def create_answers_section(df):
     st.title("Main Questions Answers")
-
     st.header("First Round")
+    
     st.subheader("How many bikes are being sold by their owners and how many bikes are being sold by distributors?")
 
     st.subheader("How many bikes are being sold are bikes from a unique owner?")
