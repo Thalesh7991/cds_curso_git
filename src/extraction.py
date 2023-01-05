bc1f16a5a5d441b60f2286b8e43a447487e71aa3
Thales Henrique <thalesh1997@gmail.com>
2023-01-05 16:30:31 -0300

    Ajuste final Projeto

--- src/extraction.py
+++ src/extraction.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import numpy as np
-import streamlit as st
+
 
 def load_data():
-    return pd.read_csv('data/processed/bikes_completed.csv')
+    return pd.read_csv("data/bikes_completed.csv")
